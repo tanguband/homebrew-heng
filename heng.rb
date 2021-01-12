1649ab8b0314bb046ee40766f4f2973572a38f92
tanguband <tanguband@gmail.com>
2021-01-13 01:02:53 +0900

    210113

--- heng.rb
+++ heng.rb
@@ -11,18 +11,13 @@ class Heng < Formula
   depends_on "pkg-config"
 
   def install
-    system "./bootstrap"
-    system "./configure","--prefix=#{prefix}","--enable-xft"
-    system "make","install"
-    bin.install "./src/hengband" => "heng-exe"
-    bin.install "./formula/heng"
-    bin.install "./formula/heng-delete"
-    bin.install "./formula/heng-dir"
-    bin.install "./formula/heng-help"
-    bin.install "./formula/heng-set"
-    bin.install "./formula/heng-up"
-    prefix.install "./formula/hengband.sh"
-    prefix.install "./lib"
+    bin.install "heng"
+    bin.install "heng-delete"
+    bin.install "heng-dir"
+    bin.install "heng-help"
+    bin.install "heng-install"
+    bin.install "heng-update"
+    prefix.install "heng.config"
   end
 
   test do
